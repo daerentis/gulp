9e59f12430a263a971ece7a3be34eb3e5a468165
daerentis <chris@strausbach.com>
2019-04-26 13:58:20 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
-test
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+function js() {
+  return src([
+      'node_modules/...',
+      'src/js/app.js'
+    ])
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglify())
+    .pipe(dest('dist/'));
+};
+
+function css() {
+  return src([
+      'node_modules/...',
+      'src/scss/main.scss'
+    ])
+    .pipe(concat('bundle.min.css'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(dest('dist/'));
+};
+
+watch('src/scss/**/*.scss', css);
+watch('src/js/*.js', js);
+
+exports.default = series(js, css);
